d4cd91cc889d981043083a60eec9ec934c4a94ae
Jamie Brough <jamie1612@gmail.com>
2019-02-18 09:25:40 +0000

    fix typo

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http'),
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({targe: process.env.TARGET_URL}).listen(process.env.PORT); // See (†)
+httpProxy.createProxyServer({target: process.env.TARGET_URL}).listen(process.env.PORT); // See (†)
